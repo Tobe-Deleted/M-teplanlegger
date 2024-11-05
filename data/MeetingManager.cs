ac4376a2a8ec2473590af62caf2a3174751d98e4
oso082024 <oso082024@gmail.com>
2024-11-05 12:27:03 +0100

    json serializing started

--- data/MeetingManager.cs
+++ data/MeetingManager.cs
@@ -1,7 +1,28 @@
+using System.Net.Mail;
+using System.Text.Json.Serialization;
+using System.Xml;
+using System.Text.Json;
+using Newtonsoft.json; // TODO: Find alternative to newtonsoft.json
+
 namespace MeetingManager.Data
 {
     public class SaveLoadMeeting
     {
         private readonly string filepath = "data/meetings.json";
+
+        public List<Meetings> LoadMeeting()
+        {
+            if (!File.Exists(filepath))
+                return new List<Meetings>();
+
+            string json = File.ReadAllText(filepath);
+            return JsonConvert.DeserializeObject<List<Meetings>>(json) ?? new List<Meetings>();
+        }
+
+        public void SaveMeeting(List<Meetings> meetings)
+        {
+            string json = JsonConvert.SerializeObject(meetings, Formatting.Intended);
+            File.WriteAllText(filepath, json);
+        }
     }
 }
